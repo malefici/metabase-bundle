7351082bebde34a01ac2a98eafada20e25a2d6cd
Malefici <malefici@users.noreply.github.com>
2024-03-14 04:45:56 +0200

    Tests

--- tests/MetabaseBundleTest.php
+++ tests/MetabaseBundleTest.php
@@ -18,20 +18,20 @@ class MetabaseBundleTest extends TestCase
 
     public function testLinkGeneratorForQuestion(): void
     {
-        $linkGenerator = new UrlGenerator('http://example.org', 'secret');
+        $linkGenerator = new UrlGenerator('http://example.org', 'secret', '+1 hour', true, true, 'light');
 
         $this->assertEquals(
-            'http://example.org/embed/question/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyZXNvdXJjZSI6eyJxdWVzdGlvbiI6MX0sInBhcmFtcyI6e30sImV4cCI6eyJkYXRlIjoiMjAyNC0wMS0wMSAwMDowMDowMS4wMDAwMDAiLCJ0aW1lem9uZV90eXBlIjozLCJ0aW1lem9uZSI6IlVUQyJ9fQ.TV7zfbC7U0TZ_hdV98YkWvTVZ7wd93kyLT1swxAjKO4',
+            'http://example.org/embed/question/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyZXNvdXJjZSI6eyJxdWVzdGlvbiI6MX0sInBhcmFtcyI6e30sImV4cCI6eyJkYXRlIjoiMjAyNC0wMS0wMSAwMDowMDowMS4wMDAwMDAiLCJ0aW1lem9uZV90eXBlIjozLCJ0aW1lem9uZSI6IlVUQyJ9fQ.TV7zfbC7U0TZ_hdV98YkWvTVZ7wd93kyLT1swxAjKO4#bordered=1&titled=1',
             $linkGenerator->generate(EmbeddingTypeEnum::question, 1, [], new \DateTimeImmutable('2024-01-01 00:00:01'))
         );
     }
 
     public function testLinkGeneratorForDashboard(): void
     {
-        $linkGenerator = new UrlGenerator('http://example.org', 'secret');
+        $linkGenerator = new UrlGenerator('http://example.org', 'secret', '+1 hour', true, true, 'light');
 
         $this->assertEquals(
-            'http://example.org/embed/dashboard/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyZXNvdXJjZSI6eyJkYXNoYm9hcmQiOjF9LCJwYXJhbXMiOnsiaWQiOjF9LCJleHAiOnsiZGF0ZSI6IjIwMjQtMDEtMDEgMDA6MDA6MDEuMDAwMDAwIiwidGltZXpvbmVfdHlwZSI6MywidGltZXpvbmUiOiJVVEMifX0.Kr-y21F0wztFeEha_1LIUasDApBlh3Wze_CE_0cNe_U',
+            'http://example.org/embed/dashboard/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyZXNvdXJjZSI6eyJkYXNoYm9hcmQiOjF9LCJwYXJhbXMiOnsiaWQiOjF9LCJleHAiOnsiZGF0ZSI6IjIwMjQtMDEtMDEgMDA6MDA6MDEuMDAwMDAwIiwidGltZXpvbmVfdHlwZSI6MywidGltZXpvbmUiOiJVVEMifX0.Kr-y21F0wztFeEha_1LIUasDApBlh3Wze_CE_0cNe_U#bordered=1&titled=1',
             $linkGenerator->generate(EmbeddingTypeEnum::dashboard, 1, ['id' => 1], new \DateTimeImmutable('2024-01-01 00:00:01'))
         );
     }
